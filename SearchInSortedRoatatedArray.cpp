608f82fe0f1f74cf9ead69da75407cf9da393cbc
Srish Srinivasan <srishwap4@gmail.com>
2021-08-25 15:44:18 +0530

    Minimal Change

--- SearchInSortedRoatatedArray.cpp
+++ SearchInSortedRoatatedArray.cpp
@@ -12,7 +12,31 @@ int fun(vector<int> arr, int x)
         {
             return mid;
         }
+
+        if(arr[low] < arr[mid])
+        {
+            if(x >= arr[low] && x < arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if(x > arr[mid] && x <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
     }
+    return -1;
 }
 
 
